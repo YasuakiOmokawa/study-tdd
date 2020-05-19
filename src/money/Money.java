ca7b3ff2330aca68cff14493ab67e9afe9dc5594
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-19 21:41:39 +0900

    Oh, detected red bar!

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,13 +1,15 @@
 package money;
 
-abstract class Money {
+class Money {
   protected int amount;
   protected String currency;
   Money(int amount, String currency) {
     this.amount = amount;
     this.currency = currency;
   }
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return null;
+  }
   String currency() {
     return currency;
   };
@@ -15,6 +17,9 @@ abstract class Money {
     Money money = (Money) object;
     return amount == money.amount
         && getClass().equals(money.getClass());
+  }
+  public String toString() {
+    return amount + " " + currency;
   }
   static Money dollar(int amount) {
     return new Dollar(amount, "USD");
